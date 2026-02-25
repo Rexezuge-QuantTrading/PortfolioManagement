ece8930d74f1f70c67ff0029a54e2ff794cf8f61
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-02-25 14:32:42 +0000

    FEAT: populate repo with npm

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.node } },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+]);
