9ad9a6d44eb82400b20643c5da03d3e36354ce52
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-02-27 16:06:16 +0000

    FIX: add types

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
-app = FastAPI()
+app: FastAPI = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"status": "ok"}
 
+
 @app.get("/hello/{name}")
 async def hello(name: str):
     return {"message": f"Hello {name}"}
 
+
 # Lambda 入口
-handler = Mangum(app)
+handler: Mangum = Mangum(app)
