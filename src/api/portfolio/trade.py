f46e65738a8d51ef6d1f790b9b639b4a5691fceb
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-02-27 16:55:53 +0000

    FEAT: create easytrader remote-client

--- src/api/portfolio/trade.py
+++ src/api/portfolio/trade.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from src.schema.Trade import TradeRequest, TradeResponse
+from easytrader.remoteclient import RemoteClient, use  # type: ignore
 
 router: APIRouter = APIRouter()
 
@@ -16,6 +17,19 @@ def trade(
     Create a new user.
     """
     print(f"request: {request}")
+    user: RemoteClient = use(
+        "ths", host="127.0.0.1", port=1430
+    )  # pyright: ignore[reportUnknownVariableType]
+    # user.prepare(
+    #     user='资金账号',
+    #     password='交易密码',
+    #     exe_path=r'C:\同花顺路径\xiadan.exe'  # 同花顺下单程序路径
+    # )
+    # # 买入
+    # user.buy('600000', price=10.5, amount=100)
+
+    # # 卖出
+    # user.sell('600000', price=10.8, amount=100)
     response = TradeResponse()
 
     if not response:
