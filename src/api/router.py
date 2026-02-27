b30b145ad5fa117a5b683e9a8767b2b5b65a526e
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-02-27 16:14:33 +0000

    FEAT: easytrader deps

---
+++ src/api/router.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+from app.api.api_v1.endpoints import user, item
+
+api_router = APIRouter()
+api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(item.router, prefix="/items", tags=["items"])
